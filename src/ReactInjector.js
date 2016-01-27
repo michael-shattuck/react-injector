d50640bfa9f10c8add024dc9dada83beab100159
= <=>
2016-01-27 08:46:58 -0700

    began fleshing out react injector

---
+++ src/ReactInjector.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+let createElement = (component, props) => {
+
+}
+
+export default class ReactInjector extends Component {
+    componentWillMount() {
+        React
+    }
+
+    render() {
+        return React.createElement({
+            ...props,
+            components,
+            createElement
+        })
+    }
+}
